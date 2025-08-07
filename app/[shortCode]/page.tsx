cb2e702a1c849cc52c4b2b5bb248930494f8bf77
Danny A. Mosquera <danny.mosquera.1099@gmail.com>
2025-08-07 15:00:57 -0500

    shortcode page

--- app/[shortCode]/page.tsx
+++ app/[shortCode]/page.tsx
@@ -26,7 +26,7 @@ export default async function RedirectPage({ params }: PageProps) {
       <div className='min-h-screen flex items-center justify-center bg-gray-50'>
         <div className='text-center'>
           <h1 className='text-2xl font-bold text-gray-900 mb-2'>Link Inactive</h1>
-          <p className='text-gray-600'>This short URL is currently inactive.</p>
+          <p className='text-gray-600 text-sm'>This short URL is currently inactive.</p>
         </div>
       </div>
     )
