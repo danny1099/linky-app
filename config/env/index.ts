4ca6e70933b9b2226ed2fe821184687fb782198b
Danny Alexander Mosquera <78458322+danny1099@users.noreply.github.com>
2025-10-08 11:56:37 -0500

    Update index.ts

--- config/env/index.ts
+++ config/env/index.ts
@@ -1,1 +1,5 @@
 export const { DATABASE_URL, BASE_URL } = process.env
+
+export const SUPABASE_URL = "https://eacnbtmtqgodlvbubxlp.supabase.co";
+export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhY25idG10cWdvZGx2YnVieGxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1OTc1MzQsImV4cCI6MjA3MDE3MzUzNH0.XD7IhnGr1X5gnlgfrBKWDI5EnfSeoV2P-oQWgLApfW4";
+
